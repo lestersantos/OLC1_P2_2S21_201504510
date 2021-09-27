5e48ddb6b007e58fe8b60185f9699a62bc59b9e6
Lester Efrain Ajucum Santos <lestersantos271@gmail.com>
2021-09-26 19:15:46 -0600

    Running web server with nodejs and typescript

--- Syscompiler/Server/app/app.js
+++ Syscompiler/Server/app/app.js
@@ -1,15 +1,10 @@
-import express = require('express');
+const express = require('express');
 
 const app = express();
 
 app.get('', (req, res) => {
 
     res.send("Hello Express!");
-})
-
-app.get('/help', (req, res) => {
-
-    res.send('Help page');
 })
 
 app.listen(3000, () => {
